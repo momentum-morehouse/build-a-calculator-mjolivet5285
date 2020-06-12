d2af1c40eabd9e4e81737b1e9d537bfa8606f391
Jolivet <mjolivet@parkrowmedia.com>
2020-06-12 03:16:31 +0000

    complet

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-
+function dis (value){
+    document.getElementById("edu").value+=value;
+    }
+    
+    function solve(){
+    let x = document.getElementById("edu").value;
+    let y = eval(x);
+    document.getElementById("edu").value = y;
+    }
+    
+    function clr(){
+    document.getElementById("edu").value = ""
+    }
